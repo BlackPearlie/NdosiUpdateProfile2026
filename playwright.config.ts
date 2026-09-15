5707c5467eb701364ff527da4a6425840dd04478
BlackPearlie <mpho94ntla@gmail.com>
2026-09-15 09:59:22 +0200

    Fixed headed mode

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   outputDir: 'test-results',
   reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],
   use: {
-    headless: false,
+    headless: Boolean(process.env.CI),
     screenshot: 'only-on-failure',
     trace: 'on-first-retry',
     video: 'retain-on-failure',
