d9edc4987db1614904845fa396e273ae375cb258
BlackPearlie <mpho94ntla@gmail.com>
2026-09-18 15:05:46 +0200

    Update test data

--- src/utils/testData.ts
+++ src/utils/testData.ts
@@ -2,5 +2,5 @@ export const testData = {
   baseUrl: process.env.BASE_URL || 'https://ndosisimplifiedautomation.vercel.app/',
   username: process.env.NDOSI_USERNAME || 'mpho@gmail.com',
   password: process.env.NDOSI_PASSWORD || '1234567890@',
-  profileImagePath: process.env.PROFILE_PICTURE || 'assets/wallpaper.jpg',
+  profileImagePath: process.env.PROFILE_PICTURE || 'assets/mpho.jpg',
 };
